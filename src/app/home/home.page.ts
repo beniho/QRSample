b5865e5fce987d55d14baf14966b27fea640c53c
beniho <ju-mi.oc3_dgjv@i.softbbank.jp>
2020-10-19 16:16:08 +0900

    initialcommit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
+declare var QRCodeStyling: any;
+declare var html2canvas: any;
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,49 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  // QRコードテキスト
+  qrtext: string = ""
+
+  constructor(private socialSharing: SocialSharing) {}
+
+  /* -------------------------------
+  QRコードを表示
+  ------------------------------- */
+  disp() {
+    
+    // QRコード表示済みの場合はクリア
+    if (document.getElementById("canvas").firstChild) {
+      document.getElementById("canvas").removeChild(document.getElementById("canvas").firstChild)
+    }
+    
+    // QRコード作成
+    const qrCode = new QRCodeStyling({
+      width: 250,
+      height: 250,
+      data: this.qrtext,
+      image: "../assets/logo-instagram.svg",
+      dotsOptions: {
+        color: "#f13f79",
+        type: "dots"
+      },
+      imageOptions: {
+        imageSize: 0.3,
+      }
+    });
+
+    // QRコード表示
+    qrCode.append(document.getElementById("canvas"));
+  }
+
+  /* -------------------------------
+  QRコードをシェア
+  ------------------------------- */
+  share() {
+    // HTML→canvas変換
+    html2canvas(document.getElementById("QR")).then(canvas => {
+      // 変換したcanvasをシェア
+      this.socialSharing.share("","",canvas.toDataURL());
+    });
+  }
 
 }
